e1f4e2b8ad97e0cfca564b54a1a13852eed3ce74
Lubasha <parakhonya1@yandex.ru>
2024-03-25 22:24:59 +0300

    change

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,25 @@
+const countTime  = function(){
+    const time = document.getElementById("timer");
+  
+    if (time.textContent >= 1){
+        time.textContent --;
+    }	else if(time.textContent = '0'){
+        alert("Время кончилось!");
+    }
+  }
+  
+  setInterval(countTime,1000); 
+  
+  //Повышенный уровень сложности #2
+  
+  /*const countTime  = function(){
+    const time = document.getElementById("timer");
+  
+    if (time.textContent >= 1){
+        time.textContent --;
+    }	else if(time.textContent = '0'){
+        location.assign("https://netology.ru/");
+    }
+  }
+  
+  setInterval(countTime,1000); */
---
+++ js-features/mole-game/task.js
@@ -0,0 +1,21 @@
+const element = index => document.getElementById("hole${index}")
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+function countClick(){
+    if(dead.textContent === 10){
+        alert("Победа!")
+    } else if(lost.textContent === 5){
+        alert("Вы проиграли")
+    }else{
+        for(let i = 1; i <= 9; i++){
+            element(i).onclick = function(){
+                if(element(i).className === "hole hole_has-mole"){
+                    dead.textContent++
+                }else{
+                    lost.textContent++
+                }
+            }
+        }
+}
+}

a6b2a7825034613b11ee8e854399d1ee50063b98
Lubasha <parakhonya1@yandex.ru>
2024-03-28 22:48:31 +0300

    2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,16 +1,16 @@
-const element = index => document.getElementById('hole${index}')
+const element = index => document.getElementById(`hole${index}`)
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
 (function countClick(){
     if(dead.textContent === 10){
-        dead.textContent = 0
-        lost.textContent = 0
         alert("Победа!")
-    } else if(lost.textContent === 5){
         dead.textContent = 0
-        lost.textContent = 0
+        dead.textContent =0
+    } else if(lost.textContent === 5){
         alert("Вы проиграли")
+        dead.textContent = 0
+        dead.textContent =0
     }else{
         for(let i = 1; i <= 9; i++){
             element(i).onclick = function(){

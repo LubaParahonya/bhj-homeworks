b281ef04466e8cba022398e947e05bdbcb1f735b
Lubasha <parakhonya1@yandex.ru>
2024-05-02 21:44:31 +0300

    progress

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,21 +1,24 @@
-const form = document.getElementById('form');
-form.addEventListener('sabmit', event => {
+const send = document.getElementById('send');
+const inputFile = document.getElementById('file')
+
+  send.addEventListener('click', event => {
     event.preventDefault();
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === xhr.DONE){
-            let i = 0
-            const progress = document.getElementById( 'progress' );
-              setInterval(function(){
-                if(i < 1){
-                  i += 0.1
-                  progress.value = i
-                }
-            }, 1000)
-        }
-    } )
-
+    xhr.upload.onprogress = () =>{
+      let i = 0
+      const progress = document.getElementById( 'progress' );
+        setInterval(() => {
+          if(i < 1){
+            i += 0.1
+            progress.value = i
+          }
+      }, 100)
+    }
+    
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
-    const formData = new formData(form)
+    const formData = new FormData(form)
     xhr.send(formData)
 })
+
+
+
